b3ac95c5dcc91db24e89f485ae11dc5e84a7d224
Irwing Naranjo <khalisser@gmail.com>
2023-05-08 09:46:34 -0400

    fix importaciones y uso de componentes inexistentes

--- client-benkyo-devs/src/components/header/Header.jsx
+++ client-benkyo-devs/src/components/header/Header.jsx
@@ -1,11 +1,12 @@
-import Logo from "./logo"
-import UserInfo from "./../user/user-info"
+// import Logo from "./logo"
+// import UserInfo from "./../user/user-info"
 
 const Header = () => {
     return (
         <header style={{display:'flex'}}>
-            <UserInfo />
-            <Logo />
+            <h1>Header</h1>
+            {/* <UserInfo /> */}
+            {/* <Logo /> */}
         </header>
     )
 }
