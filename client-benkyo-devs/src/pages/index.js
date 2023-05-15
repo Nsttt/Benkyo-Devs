cbbd273fced69ffa87a6a7afbd169ddd50d9be94
diango <yango_tascon@hotmail.com>
2023-05-15 12:57:49 +0200

    elimino componenete card de main

--- client-benkyo-devs/src/pages/index.js
+++ client-benkyo-devs/src/pages/index.js
@@ -3,22 +3,20 @@ import Header from "../components/header/Header";
 import Card from "../components/card/Card";
 
 export default function Home() {
-	return (
-		<>
-			<Head>
-				<title>Benkyo Devs</title>
-				<meta
-					name="description"
-					content="App de estudio de lenguajes de programacion"
-				/>
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-			<Header />
-			<main>
-				<Card />
-			</main>
-			<footer>//footer</footer>
-		</>
-	);
+  return (
+    <>
+      <Head>
+        <title>Benkyo Devs</title>
+        <meta
+          name="description"
+          content="App de estudio de lenguajes de programacion"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <main></main>
+      <footer>//footer</footer>
+    </>
+  );
 }
