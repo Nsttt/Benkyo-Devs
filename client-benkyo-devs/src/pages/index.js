bc80d6dbb30e28ccf68d1903ebf8b5a00e834461
Irwing Naranjo <khalisser@gmail.com>
2023-05-17 14:35:55 -0400

    remove dist tailwind

--- client-benkyo-devs/src/pages/index.js
+++ client-benkyo-devs/src/pages/index.js
@@ -13,7 +13,6 @@ export default function Home() {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-        <link href="/dist/output.css" rel="stylesheet"></link>
       </Head>
       <Header />
       <main></main>
