1d52341fde1f631e1b0099f2e2b22f40fd89e192
Lourdes <dolsleon@outlook.es>
2023-05-19 19:33:45 +0200

    funcionalidad reverso de la card

--- client-benkyo-devs/mocks/copy-card.jsx
+++ client-benkyo-devs/mocks/copy-card.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 const Card = ({ dato }) => {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleClick = () => {
+    setIsFlipped(!isFlipped);
+  };
+
   return (
     <>
       <h1>{dato.id}</h1>
       <p>{dato.pregunta}</p>
       <p>{dato.opciones[1]}</p>
       <p>{dato.opciones[0]}</p>
+      <section
+        className={`card ${isFlipped ? "flipped" : ""}`}
+        onClick={handleClick}
+      >
+        Ver respuesta
+      </section>
       <p>{dato.respuesta}</p>
       <button>siguiente Card</button>
     </>
@@ -12,5 +26,3 @@ const Card = ({ dato }) => {
 };
 
 export default Card;
-
-// crear efecto reverso en este archivo
