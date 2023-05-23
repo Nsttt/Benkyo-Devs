cb74a5bf570de6cf7e2262aeee616ca84eeeb36b
Lourdes Dols <dolsleon@outlook.es>
2023-05-23 16:01:27 +0200

    arreglos spring uno y dos

--- client-benkyo-devs/mocks/copy-card.jsx
+++ client-benkyo-devs/mocks/copy-card.jsx
@@ -1,16 +1,32 @@
+import { useState } from "react";
+
 const Card = ({ dato }) => {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleClick = () => {
+    setIsFlipped(!isFlipped);
+  };
+
   return (
-    <>
+    <section>
       <h1>{dato.id}</h1>
       <p>{dato.pregunta}</p>
       <p>{dato.opciones[1]}</p>
       <p>{dato.opciones[0]}</p>
       <p>{dato.respuesta}</p>
+
+      <div
+        className={`card ${isFlipped ? "flipped" : ""}`}
+        onClick={handleClick}
+      >
+        <div className="front">Front Content</div>
+        <div className="back">Back Content</div>
+      </div>
+
       <button>siguiente Card</button>
-    </>
+    </section>
   );
 };
 
 export default Card;
-
 // crear efecto reverso en este archivo
