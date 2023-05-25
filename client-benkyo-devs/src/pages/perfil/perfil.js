bebbce3fb0abfcaecdd365f73846ed4fb53be31a
Carol-88 <gralleta@gmail.com>
2023-05-25 10:52:50 +0200

    creo estructura grid de perfil

--- client-benkyo-devs/src/pages/perfil/perfil.js
+++ client-benkyo-devs/src/pages/perfil/perfil.js
@@ -1,38 +1,43 @@
+import UserInfo from 'carol-88/components/header/user/user-info'
+import AboutMe from 'carol-88/components/perfil/aboutme/about-me'
 import React from 'react'
 
 function perfil() {
   return (
-  
-        <div>
-            <section id='user'>
-                {/* //Componente user */}
+            <div className='grid grid-cols-3 gap-5 grid-rows-4'>
+            <section id='user' className=''>
+                <UserInfo/>
             </section>
 
-            <section id='points'>
-                {/* //Componente points */}
+            <section id='points' className='col-start-2 col-span-2'>
+                <p>points</p>
             </section>
 
-            <section id='about'>
-                {/* //Componente about */}
+            <section id='about' className='col-start-2 col-span-2'>
+               <AboutMe/>
             </section>
 
-            <section id='start'>
-                {/* Componente start */}
+            <section id='start' className='col-start-2 col-span-1'>
+                <p>start</p>
             </section>
 
-            <section id='decks'>
-                {/* //Componente decks */}
+            <section id='decks' className='col-start-1'>
+                <p>decks</p>
             </section>
 
-            <section id='saves'>
-                {/* Componente saves */}
+            <section id='saves' className='col-start-2'>
+                <p>saves</p>
             </section>
 
-            <section id='fails'>
-                {/* //Componente fails */}
+            <section id='fails' className='col-start-3'>
+                <p>fails</p>
             </section>
 
         </div>
+        
+        
+    
+        
     
   )
 }
