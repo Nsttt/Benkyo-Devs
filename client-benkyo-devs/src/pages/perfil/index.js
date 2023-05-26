7280299579dfd5d410fa31d87ddc0f6e6172118d
Carol-88 <gralleta@gmail.com>
2023-05-26 21:53:56 +0200

    intento solucionar breakpoints, no lo consigo

--- client-benkyo-devs/src/pages/perfil/index.js
+++ client-benkyo-devs/src/pages/perfil/index.js
@@ -4,32 +4,32 @@ import React from 'react'
 
 function perfil() {
   return (
-            <div className='grid grid-cols-3 gap-5 grid-rows-4 p-8'>
-            <section id='user' className=''>
+            <div className='grid grid-cols-1 lg:grid-cols-3  gap-5 lg:grid-rows-4 p-8'>
+            <section id='user' className='bg-orange-300 col-start-1 w-full h-full '>
                 <UserInfo/>
             </section>
 
-            <section id='points' className='col-start-2 col-span-2'>
+            <section id='points' className='bg-orange-300 col-start-2 col-span-2 box-border w-full h-full'>
                 <p>points</p>
             </section>
 
-            <section id='about' className='col-start-2 col-span-2'>
+            <section id='about' className='bg-orange-300 col-start-2 col-span-2 w-full h-full'>
                <AboutMe/>
             </section>
 
-            <section id='start' className='col-start-2 col-span-1'>
+            <section id='start' className='bg-orange-300 col-start-2 col-span-1 w-full h-full'>
                 <p>start</p>
             </section>
 
-            <section id='decks' className='col-start-1'>
+            <section id='decks' className='bg-orange-300 col-start-1 w-full h-full'>
                 <p>decks</p>
             </section>
 
-            <section id='saves' className='col-start-2'>
+            <section id='saves' className='bg-orange-300 col-start-2 w-full h-full'>
                 <p>saves</p>
             </section>
 
-            <section id='fails' className='col-start-3'>
+            <section id='fails' className='bg-orange-300 col-start-3 w-full h-full'>
                 <p>fails</p>
             </section>
 
