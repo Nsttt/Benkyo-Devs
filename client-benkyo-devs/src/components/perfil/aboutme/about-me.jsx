776fd0c63655d94a6de9c99e6d0f3d3112578793
Carol-88 <gralleta@gmail.com>
2023-05-27 12:30:09 +0200

    creo logica componente aboutme, faltan los estilos

--- client-benkyo-devs/src/components/perfil/aboutme/about-me.jsx
+++ client-benkyo-devs/src/components/perfil/aboutme/about-me.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-function AboutMe() {
+const AboutMe = () => {
+  const [aboutText, setAboutText] = useState(''); // Estado para almacenar el texto del perfil
+  const [editing, setEditing] = useState(false); // Estado para controlar si se está editando o no
+
+  // Manejador de cambios en el texto del perfil
+  const handleInputChange = (event) => {
+    setAboutText(event.target.value);
+  };
+
+  // Manejador de clic en el botón "Modificar"
+  const handleEditClick = () => {
+    setEditing(true);
+  };
+
+  // Manejador de clic en el botón "Guardar"
+  const handleSaveClick = () => {
+    setEditing(false);
+  };
+
   return (
-    <div>AboutMe</div>
-  )
-}
+    <div>
+      <h2>About Me</h2>
+      {editing ? (
+        <div>
+          <textarea
+            value={aboutText}
+            onChange={handleInputChange}
+            placeholder="Enter your about me text"
+          />
+          <button onClick={handleSaveClick}>Guardar</button>
+        </div>
+      ) : (
+        <div>
+          <p>{aboutText}</p>
+          <button onClick={handleEditClick}>Modificar</button>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default AboutMe
+export default AboutMe;
