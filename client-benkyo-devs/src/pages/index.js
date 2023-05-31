099b30b91aa0b9ee58c2cff943b67526ac90f5c0
Carol-88 <gralleta@gmail.com>
2023-05-31 11:45:43 +0200

    añado imagen start a home, no se ve la imagen, modifico ruta, no se ve

--- client-benkyo-devs/src/pages/index.js
+++ client-benkyo-devs/src/pages/index.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import LoginPage from './login'
 import Link from 'next/link'
+import Image from "next/image"
+
 
 
 
 function Home() {
+  const start= [
+    {
+      alt: "Start Game",
+      img: "@public/img/START.png"
+    }
+  ];
+
   return (
     <>
     <Link href="/game">
-      <button>Jugar</button>
+          <Image src={start.img} alt={start.alt} width={200} height={200}/>
     </Link>
     <LoginPage/>
     </>
