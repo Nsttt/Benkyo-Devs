55105f3a3743a00bdb926871f10785860f0f4025
Carol-88 <gralleta@gmail.com>
2023-06-05 20:16:17 +0200

    añado page registro y form registro

---
+++ client-benkyo-devs/src/pages/registro/index.js
@@ -0,0 +1,14 @@
+import Registro from "@/components/registro/Registro";
+
+
+
+export default function LoginPage() {
+  return (
+    <>
+      <main className='flex justify-center p-3 w-screen'>
+        <Registro/>
+      </main>
+    </>
+
+  );
+}
