77a031165f5611e95b804d11eee6d58c2a9a9619
Carol-88 <gralleta@gmail.com>
2023-06-06 12:46:15 +0200

    refact points

--- client-benkyo-devs/src/components/perfil/points/points.jsx
+++ client-benkyo-devs/src/components/perfil/points/points.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
+import Image from "next/image"
 
 function Points() {
         const user = {
-        username: 'Carol',
-        about: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam, voluptatum. Quisquam, voluptatum.',
-        avatar: 'https://images.unsplash.com/photo-1621574539436-4b0b0b0b0b0b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVua3lvJTIwY2Fyb2wlMjBhdmF0YXJ8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80',
         points: {
           easy: 101,
           medium: 102,
           hard: 103
         },
         logos: {
-            facil: 'img/DificultadFacil.png',
-            media: 'img/DificultadMedia.png',
-            dificil: 'img/DificultadDificil.png'
+            facil: '/img/DificultadFacil.png',
+            media: '/img/DificultadMedia.png',
+            dificil: '/img/DificultadDificil.png'
         }
       }
     const { points, logos } = user
@@ -24,9 +22,12 @@ function Points() {
   return (
     <div className='p-5'>
           <h3 className='font-bold'>Puntuación por nivel</h3>
-          <p><img src={facil}/>: {easy}</p>
-          <p><img src={media}/>: {medium}</p>
-          <p><img src={dificil}/>: {hard}</p>
+          <ul className='p-5'>
+            <li className='flex items-center'><Image src={facil} width={50} height={50}/>: {easy}</li>
+            <li className='flex items-center'><Image src={media} width={50} height={50}/>: {medium}</li>
+            <li className='flex items-center'><Image src={dificil} width={50} height={50}/>: {hard}</li>
+          </ul>
+          
       </div>
   )
 }
