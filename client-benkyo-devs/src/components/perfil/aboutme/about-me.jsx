827d5e6f5bc63efe3cc92596e3f3e0273ea7c53d
Carol-88 <gralleta@gmail.com>
2023-06-07 18:17:17 +0200

    extraigo comp aboutme de page perfil

--- client-benkyo-devs/src/components/perfil/aboutme/about-me.jsx
+++ client-benkyo-devs/src/components/perfil/aboutme/about-me.jsx
@@ -1,76 +1,168 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 
-const AboutMe = ({ currentUser }) => {
-  const [aboutText, setAboutText] = useState('');
+// const AboutMe = ({ currentUser }) => {
+//   const [aboutText, setAboutText] = useState('');
+//   const [editing, setEditing] = useState(false);
+
+//   useEffect(() => {
+//     fetchAboutText();
+//   }, []);
+
+//   const fetchAboutText = async () => {
+//     try {
+//       const response = await fetch('/api/perfil/about');
+//       const data = await response.json();
+//       const { text } = data;
+//       setAboutText(text);
+//     } catch (error) {
+//       console.log('Error al obtener el texto del perfil', error);
+//     }
+//   };
+
+//   const saveAboutText = async () => {
+//     try {
+//       await fetch('/api/perfil/about', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({ text: aboutText })
+//       });
+//       console.log('Texto del perfil guardado exitosamente');
+//     } catch (error) {
+//       console.log('Error al guardar el texto del perfil', error);
+//     }
+//   };
+
+//   const handleInputChange = (event) => {
+//     setAboutText(event.target.value);
+//   };
+
+//   const handleEditClick = () => {
+//     setEditing(true);
+//   };
+
+//   const handleSaveClick = () => {
+//     setEditing(false);
+//     saveAboutText();
+//   };
+
+//   return (
+//     <div className='m-4'>
+//       <h2 className='p-1'>About Me</h2>
+//       {editing ? (
+//         <div>
+//           <textarea
+//           className='m-4'
+//             value={aboutText}
+//             onChange={handleInputChange}
+//             placeholder="Escribe algo sobre tí"
+//           />
+//           <button onClick={handleSaveClick} className='border-2 p-1 text-white bg-cyan-600 align-bottom rounded-lg shadow-lg float-right mb-2'>Guardar</button>
+//         </div>
+//       ) : (
+//         <div>
+//           <p className='text-sky-800 p-2'>{aboutText}</p>
+//           {/* {currentUser && currentUser.id === profileOwnerId && ( */}
+//           <button onClick={handleEditClick} className='border-2 p-1 text-white bg-cyan-600 flex flex-row align-bottom rounded-lg shadow-lg float-right mb-2'>Modificar</button>
+//            {/* )} */}
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default AboutMe;
+
+const AboutMe = () => {
+
+
+  
   const [editing, setEditing] = useState(false);
+  // const [aboutText, setAboutText] = useState();
 
-  useEffect(() => {
-    fetchAboutText();
-  }, []);
+  // useEffect(() => {
+  //   fetchAboutText();
+  // }, []);
 
-  const fetchAboutText = async () => {
-    try {
-      const response = await fetch('/api/perfil/about');
-      const data = await response.json();
-      const { text } = data;
-      setAboutText(text);
-    } catch (error) {
-      console.log('Error al obtener el texto del perfil', error);
-    }
-  };
+  // const fetchAboutText = async () => {
+  //   try {
+  //     const response = await fetch('/api/perfil/about');
+  //     const data = await response.json();
+  //     const { text } = data;
+  //     setAboutText(text);
+  //   } catch (error) {
+  //     console.log('Error al obtener el texto del perfil', error);
+  //   }
+  // };
 
-  const saveAboutText = async () => {
-    try {
-      await fetch('/api/perfil/about', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ text: aboutText })
-      });
-      console.log('Texto del perfil guardado exitosamente');
-    } catch (error) {
-      console.log('Error al guardar el texto del perfil', error);
-    }
-  };
+  // const saveAboutText = async () => {
+  //   try {
+  //     await fetch('/api/perfil/about', {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-Type': 'application/json'
+  //       },
+  //       body: JSON.stringify({ text: aboutText })
+  //     });
+  //     console.log('Texto del perfil guardado exitosamente');
+  //   } catch (error) {
+  //     console.log('Error al guardar el texto del perfil', error);
+  //   }
+  // };
 
-  const handleInputChange = (event) => {
-    setAboutText(event.target.value);
-  };
+  // const handleInputChange = (event) => {
+  //   setAboutText(event.target.value);
+  // };
 
-  const handleEditClick = () => {
-    setEditing(true);
-  };
+  // const handleEditClick = () => {
+  //   setEditing(true);
+  // };
 
-  const handleSaveClick = () => {
-    setEditing(false);
-    saveAboutText();
-  };
+  // const handleSaveClick = () => {
+  //   setEditing(false);
+  //   saveAboutText();
+  // };
 
   return (
-    <div className='m-4'>
-      <h2 className='p-1'>About Me</h2>
-      {editing ? (
-        <div>
-          <textarea
-          className='m-4'
-            value={aboutText}
-            onChange={handleInputChange}
-            placeholder="Escribe algo sobre tí"
-          />
-          <button onClick={handleSaveClick} className='border-2 p-1 text-white bg-cyan-600 align-bottom rounded-lg shadow-lg float-right mb-2'>Guardar</button>
-        </div>
-      ) : (
-        <div>
-          <p className='text-sky-800 p-2'>{aboutText}</p>
-          {/* {currentUser && currentUser.id === profileOwnerId && ( */}
-          <button onClick={handleEditClick} className='border-2 p-1 text-white bg-cyan-600 flex flex-row align-bottom rounded-lg shadow-lg float-right mb-2'>Modificar</button>
-           {/* )} */}
-        </div>
-      )}
-    </div>
+    <section id='user' className='shadow-lg p-3'>
+      <UserInfo />
+      <div className='p-5'>
+        {
+          editing 
+            ? <ProfileForm user={user} setEditing={setEditing} />
+            : <ProfileInfo user={user} setEditing={setEditing} />
+        }
+      </div>
+      <hr />
+      <div>
+        <Points/>
+      </div>
+    </section>
+
+
+    // <div className='m-4'>
+    //   <h2 className='p-1'>About Me</h2>
+    //   {editing ? (
+    //     <div>
+    //       <textarea
+    //       className='m-4 border-2 p-1 text-sky-800 bg-white align-bottom rounded-lg shadow-lg float-right mb-2'
+    //         value={aboutText}
+    //         onChange={handleInputChange}
+    //         placeholder="Escribe algo sobre tí"
+    //       />
+    //       <button onClick={handleSaveClick} className='border-2 p-1 text-white bg-cyan-600 align-bottom rounded-lg shadow-lg float-right mb-2'>Guardar</button>
+    //     </div>
+    //   ) : (
+    //     <div>
+    //       <p className='text-sky-800 p-2'>{aboutText}</p>
+    //       {/* {currentUser && currentUser.id === profileOwnerId && ( */}
+    //       <button onClick={handleEditClick} className='border-2 p-1 text-white bg-cyan-600 flex flex-row align-bottom rounded-lg shadow-lg float-right mb-2'>Modificar</button>
+    //        {/* )} */}
+    //     </div>
+    //   )}
+    // </div>
   );
 };
 
 export default AboutMe;
-
